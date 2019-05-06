373025b4188ed80b8c05b9fa906251c1f5a137bd
Ash Ketchum <k3tchum@outlook.com>
2019-05-06 15:20:06 +0100

    documentation

--- src/app/Providers/DependencyProvider.php
+++ src/app/Providers/DependencyProvider.php
@@ -4,12 +4,26 @@ namespace App\Providers;
 
 class DependencyProvider {
 
+    /**
+     * The cache to hold class names and objects.
+     */
     private static $cache = [];
 
+    /**
+     * Registers a class object to the cache array.
+     *
+     * @param string $name The name of the class to access array value later.
+     * @param object $class The class object to associate with the name.
+     */
     public static function register($name, $class) {
         self::$cache[$name] = $class;
     }
 
+    /**
+     * Returns the class object associated with the name passed.
+     *
+     * @param string $name The name of the class to return the object for.
+     */
     public function fetch($name) {
         return self::$cache[$name];
     }
